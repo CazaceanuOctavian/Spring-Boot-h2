3dbf348587371fd9d7fbdcb3b2033ae645925955
Tavi <o.cazatavian@gmail.com>
2023-10-08 10:05:02 +0300

    added delete from h2 database functionality

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -3,6 +3,7 @@ package com.ltp.gradesubmission.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +30,12 @@ public class StudentController {
     @PostMapping
     public ResponseEntity<Student> saveStudent (@RequestBody Student student) {
         return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Student> deleteStudent (@PathVariable Long id) {
+        studentService.deleteStudent(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
