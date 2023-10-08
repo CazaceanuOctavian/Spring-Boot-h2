8acee43191d5781b29194516017e53b11f0090ae
Tavi <o.cazatavian@gmail.com>
2023-10-08 09:08:58 +0300

    added save student to h2 database functionality

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,11 +1,29 @@
 package com.ltp.gradesubmission.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.service.StudentService;
 
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent (@RequestBody Student student) {
+        return new ResponseEntity<Student>(studentService.saveStudent(student), HttpStatus.CREATED);
+    }
+
+
 
 }
