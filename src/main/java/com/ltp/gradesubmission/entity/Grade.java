eee3fcacd269deaee385d9b030322d6a2fa00251
Cazaceanu Octavian <o.cazatavian@gmail.com>
2023-10-08 15:56:07 +0300

    added grade entity and oneToManny relationship with student table

--- src/main/java/com/ltp/gradesubmission/entity/Grade.java
+++ src/main/java/com/ltp/gradesubmission/entity/Grade.java
@@ -1,9 +1,37 @@
 package com.ltp.gradesubmission.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "grade")
 public class Grade {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
-    private String score;
+    @Column(name = "score", nullable = false)
+    private String score;  
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "student_id", referencedColumnName = "id")
+    private Student student;
+
+    public Grade(Long id, String score) {
+        this.id = id;
+        this.score = score;
+    }
+
+    public Grade(){
+
+    }
 
     public Long getId() {
         return this.id;
@@ -19,6 +47,14 @@ public class Grade {
 
     public void setScore(String score) {
         this.score = score;
+    }
+
+    public Student getStudent() {
+        return this.student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
 }
--- src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -1,12 +1,28 @@
 package com.ltp.gradesubmission.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ltp.gradesubmission.entity.Grade;
+import com.ltp.gradesubmission.service.GradeService;
 
 
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
-    
+
+    @Autowired
+    GradeService gradeService;
+
+    @PostMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> saveGrade(@RequestBody Grade grade, @PathVariable Long studentId, @PathVariable Long courseId) {
+        return new ResponseEntity<>(gradeService.saveGrade(grade, studentId, courseId), HttpStatus.CREATED);
+    }
 
 }
