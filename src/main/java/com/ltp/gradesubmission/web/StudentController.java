22f89733a59e7211cc1b519a64581c8f61418619
Tavi <o.cazatavian@gmail.com>
2023-10-08 09:48:14 +0300

    added read student from h2 database functionality

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -3,6 +3,8 @@ package com.ltp.gradesubmission.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +21,14 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudent (@PathVariable Long id) {
+        return new ResponseEntity<>(studentService.getStudent(id), HttpStatus.OK);
+    }
+ 
     @PostMapping
     public ResponseEntity<Student> saveStudent (@RequestBody Student student) {
-        return new ResponseEntity<Student>(studentService.saveStudent(student), HttpStatus.CREATED);
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
     }
 
 
