00456feca893123b3fab39aedaf28266ae39451d
Cazaceanu Octavian <o.cazatavian@gmail.com>
2023-10-09 14:04:43 +0300

    added cascading effects with bidirectional link between STUDENT and GRADE tables

--- src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -2,33 +2,48 @@ package com.ltp.gradesubmission.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "student")
 @Getter
 @Setter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @NoArgsConstructor
 public class Student {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @NonNull
     @Column(name = "name", nullable = false)
     private String name;
+
+    @NonNull
     @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL)
+    private List<Grade> grades;
 
 }
